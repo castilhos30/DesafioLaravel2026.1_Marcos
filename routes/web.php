c1778f95d2a43ca7ee61c281926d2fab102ab6ae
MarcosCesar <marcoscesarcz@gmail.com>
2026-02-06 22:23:29 -0300

    tentando arrumar o login

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,41 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\Customer;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+   Route::middleware(Customer::class)->group(function () {
+        Route::prefix('customer')->group(function () {
+            Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+        });
+   });
+
+   Route::middleware(Admin::class)->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
+        });
+
+    
+
+    });
+
 
 Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
 
-Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
