ee73a8dcfcae3802bcc10a0e0482591955004147
MarcosCesar <marcoscesarcz@gmail.com>
2026-02-07 14:40:52 -0300

    recuperação de senha

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -36,24 +36,34 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function (User $user) use ($request) {
+            $status = Password::broker('user')->reset($request->only('email', 'password', 'password_confirmation','token')
+             function($user) use ($request) {
                 $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
+                    'password' => bcrypt($request->password)
                 ])->save();
 
-                event(new PasswordReset($user));
-            }
-        );
+                
+             });
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
+             if($status != Password::PASSWORD_RESET){
+
+                 $status = Password::broker('admin')->reset($request->only('email', 'password', 'password_confirmation','token')
+             function($admin) use ($request) {
+                $admin->forceFill([
+                    'password' => bcrypt($request->password)
+                ])->save();
+
+                
+             });
+             }
+                $status = Password::broker('admin')->reset($request->only('email', 'password', 'password_confirmation','token')
+                function($user) use ($request) {
+                   $user->forceFill([
+                       'password' => bcrypt($request->getPassword())
+                   ])->save();
+                });
+             }
+            
         return $status == Password::PASSWORD_RESET
                     ? redirect()->route('login')->with('status', __($status))
                     : back()->withInput($request->only('email'))
--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -29,12 +29,20 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
+        $status = Password::broker('users')->sendResetLink(
+            $request->only('email')
+        );  
+
+        if($status == Password::RESET_LINK_SENT)
+            return back()->with('status', __($status));
+
+        $status = Password::broker('admin')->sendResetLink(
             $request->only('email')
         );
+        
+        if($status == Password::RESET_LINK_SENT)
+            return back()->with('status', __($status));
+
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
