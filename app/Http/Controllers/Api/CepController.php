9a60a484775f388703ba2e47e657cfde24378852
MarcosCesar <marcoscesarcz@gmail.com>
2026-02-07 16:31:15 -0300

    Api feita

---
+++ app/Http/Controllers/Api/CepController.php
@@ -0,0 +1,36 @@
+    <?php
+
+    namespace App\Http\Controllers\Api;
+
+    use App\Http\Controllers\Controller;
+    use GuzzleHttp\Client;
+    use Illuminate\Http\Request;
+
+    class CepController extends Controller
+    {
+        public function show($cep)
+        {
+            if(!preg_match('/^[0-9]{8}$/')) 
+                {
+                return response()->json(['error' => 'CEP inválido.'], 400);
+                }
+
+            $client = new Client();
+            $url = "https://viacep.com.br/ws/{$cep}/json/";
+
+            try{
+                $response = $client->get($url);
+                $data = json_decode($response->getBody(), true);
+
+                if (isset($data['erro'])) {
+                    return response()->json(['error' => 'CEP não encontrado.'], 404);
+                }
+
+                return response()->json($data);
+            } catch (\Exception $e) {
+                return response()->json(['error' => 'Erro ao consultar o CEP.'], 500);
+
+            }
+        }
+     
+     }
