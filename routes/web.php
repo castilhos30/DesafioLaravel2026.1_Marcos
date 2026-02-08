992f8415276ece153e69dbca0e1f6d20a37fff59
MarcosCesar <marcoscesarcz@gmail.com>
2026-02-08 18:35:00 -0300

    feat(View do produto individual) deixando a view pronta do produto

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/visualizar-exemplo', function () {
+    return view('produto_individual');
+});
+
 require __DIR__.'/auth.php';
