9dbb1624e051df09f6a1fa68aadd74c569da1dc2
MarcosCesar <marcoscesarcz@gmail.com>
2026-02-12 10:23:45 -0300

    feat(View do carrinho) deixando a view do carrinho funcional

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/carrinho', function () {
+    return view('carrinho');
+})->name('carrinho');
+
 require __DIR__.'/auth.php';
